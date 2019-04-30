c7ad1d258e52b417bfab501a3b0a5e069b37ff1e
Herb Caudill <herb@devresults.com>
2019-04-30 16:46:04 +0200

    components/TodoForm:

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,6 +1,6 @@
 import gql from 'graphql-tag'
-import React from 'react'
-import { Mutation } from 'react-apollo'
+import React, { useRef } from 'react'
+import { useMutation } from 'react-apollo-hooks'
 
 const ADD_TODO = gql`
   mutation addTodo($text: String!) {
@@ -10,35 +10,24 @@ const ADD_TODO = gql`
   }
 `
 
-const TodoForm = () => (
-  <Mutation mutation={ADD_TODO}>
-    {addTodo => {
-      let input
-      return (
-        <div>
-          <form
-            onSubmit={e => {
-              e.preventDefault()
-              if (!input.value.trim()) {
-                return
-              }
-              addTodo({ variables: { text: input.value } })
-              input.value = ''
-            }}
-          >
-            <input
-              className="new-todo"
-              placeholder="What needs to be done?"
-              autoFocus={true}
-              ref={node => {
-                input = node
-              }}
-            />
-          </form>
-        </div>
-      )
-    }}
-  </Mutation>
-)
+const TodoForm = () => {
+  const input = useRef(null)
+
+  const onSubmit = e => {
+    e.preventDefault()
+    if (input.current.value.trim().length === 0) return
+    addTodo({ variables: { text: input.current.value } })
+    input.current.value = ''
+  }
+
+  const [addTodo] = useMutation(ADD_TODO)
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input className="new-todo" placeholder="What needs to be done?" autoFocus={true} ref={input} />
+      </form>
+    </div>
+  )
+}
 
 export default TodoForm
