213ee9446ef2992dc924fd57c18e9f32ce742fdd
Herb Caudill <herb@devresults.com>
2019-04-30 16:45:59 +0200

    components/Link:

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Link = ({ active, children, onClick }) => {
   return (
+    // eslint-disable-next-line
     <a
       className={active ? 'selected' : ''}
-      href="#"
       onClick={e => {
         e.preventDefault()
         onClick()
