0d70f47f5a4e1b661272f487c67b8fc70c763ea6
Herb Caudill <herb@devresults.com>
2019-05-01 13:44:52 +0200

    only show clear completed button if there are completed items

--- src/components/ClearCompleted.js
+++ src/components/ClearCompleted.js
@@ -1,5 +1,22 @@
 import React from 'react'
+import { useQuery } from 'react-apollo-hooks'
+
+import { SHOW_COMPLETED } from '../constants'
+import { GET_STATE } from '../graphql'
+import { applyFilter } from '../utils'
 
 export function ClearCompleted() {
-  return <button className="clear-completed">Clear completed</button>
+  const { data } = useQuery(GET_STATE)
+  const { todos } = data
+  const completedCount = applyFilter(todos, SHOW_COMPLETED).length
+
+  if (completedCount === 0) return null
+
+  const onClick = e => {}
+
+  return (
+    <button className="clear-completed" onClick={onClick}>
+      Clear completed
+    </button>
+  )
 }
