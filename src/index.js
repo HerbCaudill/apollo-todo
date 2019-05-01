ce82e9029daf59dac80295c23ac226f71926725a
Herb Caudill <herb@devresults.com>
2019-05-01 14:52:32 +0200

    get persistence working

--- src/index.js
+++ src/index.js
@@ -2,26 +2,37 @@ import 'todomvc-app-css/index.css'
 import 'todomvc-common/base.css'
 
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { persistCache } from 'apollo-cache-persist'
 import { ApolloClient } from 'apollo-client'
 import React from 'react'
-import { ApolloProvider } from 'react-apollo'
 import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks'
 import { render } from 'react-dom'
 
 import App from './components/App'
 import { defaults, resolvers, typeDefs } from './graphql'
 
-const cache = new InMemoryCache()
+async function setDefaults(cache) {
+  cache.writeData({ data: defaults })
+}
 
-cache.writeData({ data: defaults })
+async function start() {
+  const cache = new InMemoryCache()
 
-const client = new ApolloClient({ cache, resolvers, typeDefs })
+  await persistCache({
+    cache,
+    storage: window.localStorage,
+  })
 
-render(
-  <ApolloProvider client={client}>
+  const cacheIsEmpty = !cache.data || !cache.data.data || Object.keys(cache.data.data).length === 0
+  if (cacheIsEmpty) await setDefaults(cache)
+
+  const client = new ApolloClient({ cache, resolvers, typeDefs })
+
+  render(
     <ApolloHooksProvider client={client}>
       <App />
-    </ApolloHooksProvider>
-  </ApolloProvider>,
-  document.getElementById('root')
-)
+    </ApolloHooksProvider>,
+    document.getElementById('root')
+  )
+}
+start()
